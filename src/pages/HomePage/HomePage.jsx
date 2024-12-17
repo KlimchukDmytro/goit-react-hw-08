0e1972add4d5f2d33b9e5fc54a3412c5448fb7b2
Dmytro Klimchuk <d664433334@gmail.com>
2024-12-17 20:41:35 +0200

    indicated contacts

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,11 +2,6 @@ const HomePage = () => {
   return (
     <div>
       <h1>HomePage</h1>
-      <p>ТЕКСТИ</p>
-      <p>ТЕКСТИ</p>
-      <p>ТЕКСТИ</p>
-      <p>ТЕКСТИ</p>
-      <p>ТЕКСТИ</p>
     </div>
   );
 };
